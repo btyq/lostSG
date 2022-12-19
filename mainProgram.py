18e6b9cfaf61333f99fd296451380222a3f58eb7
velpoopy <bryanttanyq98@gmail.com>
2022-12-19 19:20:59 +0800

    update on application

--- mainProgram.py
+++ mainProgram.py
@@ -1,12 +1,32 @@
 import pyautogui
-import pandas as pd
+import openpyxl
 
+class Employee:
+    def __init__(self, name):
+        self.name = name
+        
 def readFile():
-    x = pyautogui.prompt('Please enter excel timesheet filename', 'LostSG Salary Calculator')
-    x = x + ".xlsx"
-    print(x)
-    df = pd.read_excel(x, sheet_name='Punch Time')
-    print(df)
+    x = "temp"
+    try:
+        x = pyautogui.prompt('Please enter excel timesheet filename', 'LostSG Salary Calculator')
+        if x is None:
+            pass
+        else:
+            x = x + ".xlsx"
+            print(x)
+            punchTime = openpyxl.load_workbook(x)
+            punchTime.active = punchTime['Punch Time']
+            sh = punchTime.active
+            for i in range(1, sh.max_row+1):
+                print("\n")
+                print("Row ", i, " data :")
+                for j in range(1, sh.max_column+1):
+                    cell_obj = sh.cell(row=i, column=j)
+                    print(cell_obj.value, end=" ")
+
+    except FileNotFoundError:
+        pyautogui.alert(text='File not Found', title='Error', button='OK')
+
 def main():
     readFile()
 if __name__ == '__main__':
