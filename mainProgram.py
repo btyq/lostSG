3ea93c530cbaa3ed630f0de3a41d6deded1ed8ff
velpoopy <bryanttanyq98@gmail.com>
2022-12-19 15:02:34 +0800

    New update

--- mainProgram.py
+++ mainProgram.py
@@ -1,7 +1,13 @@
+import pyautogui
+import pandas as pd
 
-
+def readFile():
+    x = pyautogui.prompt('Please enter excel timesheet filename', 'LostSG Salary Calculator')
+    x = x + ".xlsx"
+    print(x)
+    df = pd.read_excel(x, sheet_name='Punch Time')
+    print(df)
 def main():
-    print("hello")
-
+    readFile()
 if __name__ == '__main__':
     main()
